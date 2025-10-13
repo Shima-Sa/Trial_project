ce5333a995277bc81cebeecd5631f4b21007a599
Shima Saberianpour <saberianpours@gmail.com>
2025-10-13 11:01:19 +0000

    time.limit

--- models/dimensions/dim_time.sql
+++ models/dimensions/dim_time.sql
@@ -1,10 +1,15 @@
 {{ config(materialized='view') }}
 
-select distinct
-  date_trunc(date(created_at), isoweek) as week_start,
-  date_trunc(date(created_at), month)   as month_start,
-  extract(year  from created_at)        as year,
-  extract(month from created_at)        as month_number,
-  format_date('%B', date_trunc(date(created_at), month)) as month_name,
-  extract(week  from created_at)        as week_number
-from {{ ref('int_question_tags') }}
+with weeks as (
+  select distinct
+    date_trunc(date(created_at), isoweek) as week_start
+  from {{ ref('int_question_tags') }}
+)
+select
+  week_start,
+  date_trunc(week_start, month)                    as month_start,
+  extract(year  from week_start)                   as year,
+  extract(month from week_start)                   as month_number,
+  format_date('%B', date_trunc(week_start, month)) as month_name,
+  extract(isoweek from week_start)                 as week_number
+from weeks
